36afb506072932f79a9a78e480eb505286e9ff9e
jshapi16 <jord.shapiro@gmail.com>
2021-07-22 12:48:30 -0700

    updating starter code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,4 @@
+//--------------------------BAR CHART SETUP------------------------//
 //get the values for the bar chart: top 10 sample values and otu ids
 
 function unpack(rows, index) {
@@ -10,12 +11,20 @@ d3.json("samples.json").then((data) => {
     //  For loop to get only the top 10 bacteria
     for (var i = 0; i < 10; i++) {
         var topTenSamples = data.samples.sample_values;
-        var otuId = data.samples.otu_ids;
-        var otuLabel = data.samples.out_labels;
+        var topTenOtuId = data.samples.otu_ids;
+        var topTenOtuLabel = data.samples.otu_labels;
         //probably i need to map the otu labels to the otu ids
         //can i call these variables outside of the loop?
     }
 });
+
+
+//-------------------------------DROP DOWN AND TABLE SETUP------------------------//
+
+//get the key and value pairs
+for (const [key, value] of Object.entries(data.metadata)) {
+    console.log(`${key}: ${value}`);
+  }
 
 //build the table for the metadata
 //variables for the dropdown
@@ -31,11 +40,41 @@ var sampleNum = data.metadata.id
 //initializes the page with a default value
 function init() {
     var trace = {
-        
+
     }
          
 
 }
+
+//--------------------BUBBLE CHART SETUP----------------//
+
+//variables for bubble chart
+var otuIds = data.samples.otu_ids
+var sampleValues = data.samples.sample_values
+var otuLabels = data.samples.otu_labels
+
+//bubble Chart setup
+var trace1 = {
+    x: otuIds,
+    y: sampleValues,
+    text: [otuLabels],
+    mode: "markers",
+    marker: {
+        color: [otuIds],
+        size: [sampleValues]
+    }
+};
+
+var data = [trace1];
+
+var layout = {
+    title: "Belly Button Bacteria",
+    showlegend = true
+};
+
+Plotly.newPlot("bubble", data, layout);
+
+
 
 // const dataPromise = d3.json(data);
 // console.log("Data Promise: ", dataPromise);

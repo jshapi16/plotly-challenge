eb22580dd68c0156f9a24db94770b414de9322f6
jshapi16 <jord.shapiro@gmail.com>
2021-07-23 10:40:59 -0700

    intializing dropdown

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,82 +1,45 @@
+//-----------------------INITIALIZED FUNCTION--------------//
+
 //--------------------------BAR CHART SETUP------------------------//
 //get the values for the bar chart: top 10 sample values and otu ids
+function createBarChart(id) {
+    console.log(`This is the bar chart for ${id}`)
+};
 
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
-
-d3.json("samples.json").then((data) => {
-    //  For loop to get only the top 10 bacteria
-    for (var i = 0; i < 10; i++) {
-        var topTenSamples = data.samples.sample_values;
-        var topTenOtuId = data.samples.otu_ids;
-        var topTenOtuLabel = data.samples.otu_labels;
-        //probably i need to map the otu labels to the otu ids
-        //can i call these variables outside of the loop?
-    }
-});
-
-
-//-------------------------------DROP DOWN AND TABLE SETUP------------------------//
-
-//get the key and value pairs
-for (const [key, value] of Object.entries(data.metadata)) {
-    console.log(`${key}: ${value}`);
-  }
-
-//build the table for the metadata
-//variables for the dropdown
-
-var age = data.metadata.age
-var bbtype = data.metadata.bbtype
-var ethnicity = data.metadata.ethnicity
-var gender = data.metadata.gender
-var location = data.metadata.location
-var wfreq = data.metadata.wfreq
-var sampleNum = data.metadata.id
-
-//initializes the page with a default value
-function init() {
-    var trace = {
-
-    }
-         
-
-}
 
 //--------------------BUBBLE CHART SETUP----------------//
-
-//variables for bubble chart
-var otuIds = data.samples.otu_ids
-var sampleValues = data.samples.sample_values
-var otuLabels = data.samples.otu_labels
-
-//bubble Chart setup
-var trace1 = {
-    x: otuIds,
-    y: sampleValues,
-    text: [otuLabels],
-    mode: "markers",
-    marker: {
-        color: [otuIds],
-        size: [sampleValues]
-    }
+//function for bubble chart
+function createBubbleChart(id) {
+    console.log(`This is the bubble chart for ${id}`)
 };
 
-var data = [trace1];
 
-var layout = {
-    title: "Belly Button Bacteria",
-    showlegend = true
+//---------------------------DISPLAY DEMOGRAPHIC DATA-------------------//
+function displayMetadata(data){
+    d3.json("samples.json").then((data) => {
+        console.log(data)
+        var metadata = data.metadata
+    });
 };
 
-Plotly.newPlot("bubble", data, layout);
+d3.selectAll("#selDataset").on("change", init);
+//initializes the page with a default value
+function init() {
+    var dropDownMenu = d3.select("#selDataset");
+    d3.json("samples.json").then((data) => {
+        var sampleNames = data.names;
+        sampleNames.forEach((sample)=> {
+            dropDownMenu
+                .append("option")
+                .text(sample)
+                .property("value", sample);
 
+        });
+        var selectedID = sampleNames[0]
+        createBarChart(selectedID);
+        createBubbleChart(selectedID);
+        displayMetadata(selectedID);
+    }); 
+};
 
-
-// const dataPromise = d3.json(data);
-// console.log("Data Promise: ", dataPromise);
-
-//div class=well for 
+init();
